529b4552227c0ce83dce729176f192b1e73d6546
aknishi <adriankawanishi@gmail.com>
2018-10-15 01:17:18 -0700

    Update gmail domain, rename variables.scss to have underscore

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,7 +40,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    domain:               'example.com',
+    domain:               'gmail.com',
     user_name:            'ctcmailertest@gmail.com',
     password:             'Sanfrancisco123',
     authentication:       'plain',
