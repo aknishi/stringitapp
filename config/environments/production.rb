a15a6004498cc26aae4adc370753c9bdfa6b1f3c
Adrian Kawanishi <adriankawanishi@gmail.com>
2018-10-16 15:09:24 -0700

    email protection

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,8 +71,8 @@ Rails.application.configure do
     address:              'smtp.gmail.com',
     port:                 587,
     domain:               'example.com',
-    user_name:            'ctcmailertest@gmail.com',
-    password:             'Sanfrancisco123',
+    user_name:            ENV["email_user_name"],
+    password:             ENV["email_password"],
     authentication:       'plain',
     enable_starttls_auto: true
   }
