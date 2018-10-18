66d122a13ef45c791e0a58e8d14351d12193a7f2
aknishi <adriankawanishi@gmail.com>
2018-10-18 15:31:27 -0700

    remove store logger and window functions

--- frontend/stringit.jsx
+++ frontend/stringit.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
-import { fetchUser } from './actions/user_actions';
-
 
 document.addEventListener('DOMContentLoaded', () => {
   let store;
@@ -19,10 +17,6 @@ document.addEventListener('DOMContentLoaded', () => {
     } else {
       store = configureStore();
     }
-  window.fetchUser = fetchUser;
-  window.fetchOrderLines = fetchOrderLines;
-  window.dispatch = store.dispatch;
-  window.getState = store.getState;
   const root = document.getElementById('root');
   ReactDOM.render(<Root store={store} />, root)
 });
