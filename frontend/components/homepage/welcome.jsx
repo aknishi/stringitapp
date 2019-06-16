38ec5cbe9bdae384840c11df749e2b7cfebc4ce6
aknishi <adriankawanishi@gmail.com>
2019-06-16 15:35:02 -0700

    Changes to homepage

--- frontend/components/homepage/welcome.jsx
+++ frontend/components/homepage/welcome.jsx
@@ -21,13 +21,13 @@ const WelcomePage = () => {
   }
   return (
     <div className="homepage">
-      <div className="court-image-container"></div>
+      <div className="court-image"></div>
       <div className="welcome">
         <h1 className="welcome__text welcome-anim">
           Welcome to String It App!
         </h1>
       </div>
-      <div>
+      <section>
         <ul className="keynotes">
           <li className="keynotes__item service-anim">
             <img src={window.grayRacketURL} />
@@ -52,24 +52,25 @@ const WelcomePage = () => {
             </div>
           </li>
         </ul>
-      </div>
-      <div className="features">
+      </section>
+      <section className="features">
         <img className="graph" src={window.graphURL} />
         <div className="keynotes__text">
           <h2>Analize your gameplay</h2>
           <h3>Gather data from previous orders</h3>
           <h3>to improve your game at the court.</h3>
         </div>
-        <div className="more-features">
-          <img src={window.userShareURL} />
-          <div className="keynotes__text">
-            <h2>Exchange information with other users</h2>
-            <h3>Work together to improve your game</h3>
-            <h3>and get tips from the experts</h3>
-          </div>
+      </section>
+      <div className="tennis-play-image"></div>
+      <section className="features">
+        <img src={window.userShareURL} />
+        <div className="keynotes__text">
+          <h2>Exchange information with other users</h2>
+          <h3>Work together to improve your game</h3>
+          <h3>and get tips from the experts</h3>
         </div>
-      </div>
-      <div className="footer">
+      </section>
+      <footer className="footer">
         <div className="footer-section">
           <div>
             <Link to="/" className="logo-link">
@@ -97,7 +98,7 @@ const WelcomePage = () => {
             <a href="#" className="fa fa-github"></a>
           </div>
         </div>
-      </div>
+      </footer>
     </div>
   )
 }
